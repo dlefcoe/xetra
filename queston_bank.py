669f8b6b552c652eb11602965902ca5d3093b374
dlefcoe <dlefcoe@gmail.com>
2025-11-30 10:59:23 +0000

    116 mod hint

--- queston_bank.py
+++ queston_bank.py
@@ -1603,9 +1603,9 @@ class QuestionBank:
             answer=["2", "3"],
             answer_type="multiple",
             comment_hint="""
-            The Continuous Auction with Specialist model is designed for less liquid securities and has unique rules. 
-            > A specialist's matching quote is a one-time trigger for price determination, 
-            > and the price is always set to maximize the volume of trades, not based on order entry time.
+            The Continuous Auction with Specialist model is designed for less liquid securities and has unique rules. \n\n
+            A specialist's matching quote is a one-time trigger for price determination, 
+            and the price is always set to maximize the volume of trades, not based on order entry time.
             """
         ),
         QA(
